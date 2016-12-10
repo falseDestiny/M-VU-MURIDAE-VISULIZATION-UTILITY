10c1b9f3d02114fb6c8a0bedb910f224667b9894
Fox51v2 <Fox51v2@yahoo.com>
2016-12-10 00:16:43 -0500

    Update server.py

--- server.py
+++ server.py
@@ -826,4 +826,4 @@ def loadMice(dataset):
     db.close()
 
 if __name__ == '__main__':
-    socketio.run(app, host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug = True)
+    socketio.run(app, host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 80)), debug = True)
