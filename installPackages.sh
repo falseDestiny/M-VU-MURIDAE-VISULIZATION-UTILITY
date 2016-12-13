03ef87f0c34466d335907704b5f396be05d2b4b3
Fox51v2 <Fox51v2@yahoo.com>
2016-12-13 10:20:57 -0500

    Package changes

--- installPackages.sh
+++ installPackages.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+ash
 # Linux/UNIX box with ssh key based login
 SERVERS="45.55.254.5"
 # Set user variable
@@ -10,19 +10,24 @@ for server in $SERVERS
 do
 	echo "The installation has began..."
 	#ssh root@159.203.132.0 "cd mvu && \i mouseDB.sql"
-	# chmod og+rX /mvu
 	#sudo sudo -u postgres psql
 	ssh ${user}@${server} "git clone https://github.com/KebertXela87/mvu.git"	
-	ssh ${user}@${server} "sudo apt-get -y update"
-	ssh ${user}@${server} "sudo apt-get -y upgrade"
-	ssh ${user}@${server} "sudo apt install -y python-pip python-pip django"
-	ssh ${user}@${server} "sudo easy_install -y flask flask markdown flask-login flask-socketio"
-	ssh ${user}@${server} "sudo apt-get install -y python-psycopg2 postgresql-contrib-9.3 postgresql python-eventlet"
-	ssh ${user}@${server} "sudo apt-get install -y postgresql-contrib flask python-pip python-dev build-essential"
+	ssh ${user}@${server} "sudo apt-get update -y"
+	ssh ${user}@${server} "sudo apt-get upgrade -y"
+	ssh ${user}@${server} "sudo apt install python-pip -y"
+	ssh ${user}@${server} "sudo apt-get install postgresql -y"
+	ssh ${user}@${server} "sudo easy_install flask"
+	ssh ${user}@${server} "sudo easy_install flask markdown"
+	ssh ${user}@${server} "sudo easy_install flask-login"
+	ssh ${user}@${server} "sudo easy_install flask-socketio"
+	ssh ${user}@${server} "sudo apt-get install python-psycopg2 -y"
+	ssh ${user}@${server} "sudo apt-get install postgresql-contrib-9.3 -y"
+	ssh ${user}@${server} "sudo apt-get install python-eventlet -y"		
+	ssh ${user}@${server} "sudo apt-get install postgresql-contrib -y"
+	ssh ${user}@${server} "sudo apt-get install python-pip python-dev build-essential -y"	
 	ssh ${user}@${server} "sudo apt-get install --upgrade pip -y"
 	ssh ${user}@${server} "sudo apt-get -y update"
 	ssh ${user}@${server} "sudo apt-get -y upgrade"	
 	ssh ${user}@${server} "sudo pip install flask_mail==0.9.0"
 	echo "The installation has finished..."	
 done
-
